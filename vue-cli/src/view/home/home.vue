840ed05878de8837012e385e39e07ef8514ca4bb
EricRu <rx001357977@163.com>
2019-05-30 19:35:48 +0800

    2019-5-30

---
+++ vue-cli/src/view/home/home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-button v-on:click="tuichu" class="right">退出</el-button>
+    <button class="big" @click="school_choose">校选修课</button>
+    <button class="big" @click="program_choose">院选修课</button>
+    <button class="big" @click="sports_choose">体育选修课</button>
+  </div>
+</template>
+
+<script>
+    export default {
+  name: "Home",
+      methods:{
+        tuichu: function () {
+          this.$router.push('/');
+        },
+      school_choose:function () {
+        this.$router.push('/school');
+        },
+      program_choose:function (){
+        this.$router.push('/program');
+      },
+      sports_choose:function () {
+        this.$router.push('/sports');
+      }
+      }
+  }
+  </script>
+
+<style scoped>
+.big{
+  width: 100px;
+  height: 100px;
+}
+</style>
+
